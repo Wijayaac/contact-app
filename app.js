2599e45852fb19d095f117f7a753104ebe053164
Wijayaac <wijayaac25@gmail.com>
2021-04-10 10:15:35 +0800

    spliting the code, make it more readable

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-const fs = require("fs");
-const { resolve } = require("path");
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-// creating data folder
-const dirPath = "./data";
-if (!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath);
-}
-
-// creating file contacts.json if not exists
-const filePath = "./data/contacts.json";
-if (!fs.existsSync(filePath)) {
-  fs.writeFileSync(filePath, "[]", "utf-8");
-}
-
-// abstract method for creating template questions
-const askQuestions = (question) => {
-  return new Promise((resolve, reject) => {
-    rl.question(question, (answer) => {
-      resolve(answer);
-    });
-  });
-};
+const { askQuestions, saveContact } = require("./contacts");
 
 // main method for input data
 const main = async () => {
@@ -32,14 +6,7 @@ const main = async () => {
   const email = await askQuestions("Masukkan email anda : ");
   const number = await askQuestions("Masukkan nomor anda : ");
 
-  const contact = { name, email, number };
-  const file = fs.readFileSync("data/contacts.json", "utf-8");
-  const contacts = JSON.parse(file);
-
-  contacts.push(contact);
-  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
-  console.log("Terimakasih sudah memasukkan data");
-  rl.close();
+  saveContact(name, email, number);
 };
 
 main();
